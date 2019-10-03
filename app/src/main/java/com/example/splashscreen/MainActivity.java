eb5df190b3008678cdec2a6702e86c8f845619b2
Saransh Jain <35698657+saransh219@users.noreply.github.com>
2019-10-03 21:49:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay2,rellay1;
+
+    Handler handler = new Handler();
+    //
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable,2000);//delay
+    }
+}
